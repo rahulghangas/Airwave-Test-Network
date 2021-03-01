a7578a819451c7c99053983d65344bef9b2c4232
Rahul Ghangas <u6157693@anu.edu.au>
2021-03-01 08:35:06 +0530

    chore: update script to zip application folder and server copy as single file

--- infra/copy_resources_script.sh
+++ infra/copy_resources_script.sh
@@ -3,6 +3,8 @@
 jq -r '[.resources[] | select(.type=="aws_eip") | .instances[] | select(.attributes.public_dns != null) | .attributes.public_dns] | .[]' terraform.tfstate > dns
 jq -r '[.resources[] | select(.type=="aws_eip") | .instances[] | select(.attributes.public_ip != null) | .attributes.public_ip] | .[]' terraform.tfstate > ../build/ip
 
+zip -r build.zip ../build/
 while read p; do
-  scp -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" -i key -r ../build ec2-user@"$p":~/
+  scp -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" -i key build.zip ec2-user@"$p":~/ &
 done <dns
+
